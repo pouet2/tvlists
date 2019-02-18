46721c45175cf4de365a176dbc38f6e0f964f5d5
Tulsene <wocpi.r@gmail.com>
2019-02-18 15:18:09 +0100

    use requests instead of httplib2, added margin list creation option, added kraken

--- update_script.py
+++ update_script.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import httplib2
-import json
+import requests
 import os
 
 
@@ -14,7 +13,8 @@ class market_lists_updater():
         self.marketplace_obj = {'poloniex': 'https://poloniex.com/public?command=returnTicker',
                                 'bittrex' : 'https://api.bittrex.com/api/v1.1/public/getmarkets',
                                 'binance' : 'https://api.binance.com/api/v1/exchangeInfo',
-                                'bitfinex': 'https://api-pub.bitfinex.com/v2/tickers?symbols=ALL'}
+                                'bitfinex': 'https://api-pub.bitfinex.com/v2/tickers?symbols=ALL',
+                                'kraken'  : 'https://api.kraken.com/0/public/AssetPairs'}
 
     def req_marketplace(self, marketplace):
         """Make request to the selected marketplace.
@@ -23,23 +23,13 @@ class market_lists_updater():
         """
         if marketplace not in self.marketplace_obj:
             raise MarketplaceSelectionError("The marketplace you ask don't exist in self.marketplace_obj.")
-        h = httplib2.Http('.cache')
-        header, content = h.request(self.marketplace_obj[marketplace])
-        data = self.format_response(content)
-        return data
+        r = requests.get(self.marketplace_obj[marketplace])
+        if r.status_code not in (200, 201, 202):
+            r.raise_for_status()
+        r = r.json()
+        return r
 
-
-    def format_response(self, resp):
-        """Read & format http request.
-        resp : byte, content returned by a http GET.
-        return : json object, formated data.
-        """
-        data = json.loads(resp.decode('utf-8'))
-        if 'error' in data:
-            raise ApiError(data['error'])
-        return data
-
-    def construct_file_path(self, marketplace):
+    def construct_file_path(self, marketplace, margin='no'):
         """Check if the directory exist and construct the file path.
         marketplace : string, marketplace name.
         return: string, constructed file path.
@@ -47,16 +37,19 @@ class market_lists_updater():
         if marketplace not in self.directories_list:
             os.makedirs(self.path_root + marketplace)
             print('The directory for ' + marketplace + ' has been created.')
-        file_path = self.path_root + marketplace + '/' + marketplace + \
-                    '_pair_list.txt'
+        if margin != 'no':
+            file_path = self.path_root + marketplace + '/' + marketplace + \
+                    '_margin_pair_list.txt'
+        else:
+            file_path = self.path_root + marketplace + '/' + marketplace + \
+                        '_pair_list.txt'
         return file_path
 
-    def compare_pairs_list(self, marketplace, pairs):
+    def compare_pairs_list(self, marketplace, file_path, pairs):
         """Check if a file for the pairs list exist and update if needed.
         marketplace : string, marketplace name.
         pairs : list, all of the actual pairs of the marketplace.
         """
-        file_path = self.construct_file_path(marketplace)
         if not os.path.isfile(file_path):
             self.write_file(file_path, pairs)
             print('Pairs list file for ' + marketplace + ' has been created.')
@@ -134,7 +127,7 @@ class market_lists_updater():
         usdc_pairs.sort()
         usdt_pairs.sort()
         xmr_pairs.sort()
-        all_pairs = btc_pairs + usdt_pairs + eth_pairs + usdc_pairs + xmr_pairs
+        all_pairs = usdt_pairs + btc_pairs + eth_pairs + usdc_pairs + xmr_pairs
         #return all_pairs # I wasn't able to return data from exec in main, need to be solved
         self.new_pairs_list = all_pairs
 
@@ -176,7 +169,7 @@ class market_lists_updater():
         eth_pairs.sort()
         usd_pairs.sort()
         usdt_pairs.sort()
-        all_pairs = btc_pairs + usdt_pairs + eth_pairs + usd_pairs
+        all_pairs = usdt_pairs + btc_pairs + eth_pairs + usd_pairs
         #return all_pairs # I wasn't able to return data from exec in main, need to be solved
         self.new_pairs_list = all_pairs
 
@@ -220,7 +213,7 @@ class market_lists_updater():
         eth_pairs.sort()
         usdt_pairs.sort()
         xrp_pairs.sort()
-        all_pairs = btc_pairs + usdt_pairs + eth_pairs + bnb_pairs + xrp_pairs
+        all_pairs = usdt_pairs + btc_pairs + eth_pairs + bnb_pairs + xrp_pairs
         #return all_pairs # I wasn't able to return data from exec in main, need to be solved
         self.new_pairs_list = all_pairs
 
@@ -287,12 +280,81 @@ class market_lists_updater():
         jpy_pairs.sort()
         usd_pairs.sort()
         xlm_pairs.sort()
-        all_pairs = btc_pairs + usd_pairs + eth_pairs + eur_pairs + jpy_pairs + \
+        all_pairs = usd_pairs + btc_pairs + eth_pairs + eur_pairs + jpy_pairs + \
                     gbp_pairs +jpy_pairs + dai_pairs + eos_pairs + xlm_pairs
-        print(all_pairs)
         #return all_pairs # I wasn't able to return data from exec in main, need to be solved
         self.new_pairs_list = all_pairs
 
+    def kraken(self, marketplace, api_json):
+        """Construct an ordered list of pairs for kraken marketplace
+           then compare it.
+           api_json : json, non ordered list from marketplace.
+           return : list, ordered list of pairs from marketplace api.
+        """
+        btc_pairs = []
+        btc_margin_pairs = []
+        cad_pairs = []
+        eth_pairs = []
+        eth_margin_pairs = []
+        eur_pairs = []
+        eur_margin_pairs = []
+        jpy_pairs = []
+        usd_pairs = []
+        usd_margin_pairs = []
+        all_pairs = []
+        all_margin_pairs = []
+        markets_dict = api_json['result']
+        for pair in markets_dict:
+            market_pair = pair
+            if pair[-3:] == 'XBT':
+                pair = marketplace.upper() + ':' + pair
+                btc_pairs.append(pair)
+                if markets_dict[market_pair]['leverage_buy'] != []:
+                    btc_margin_pairs.append(pair)
+                continue
+            if pair[-3:] == 'CAD':
+                pair = marketplace.upper() + ':' + pair
+                cad_pairs.append(pair)
+                continue
+            if pair[-3:] == 'ETH':
+                pair = marketplace.upper() + ':' + pair
+                eth_pairs.append(pair)
+                if markets_dict[market_pair]['leverage_buy'] != []:
+                    eth_margin_pairs.append(pair)
+                continue
+            if pair[-3:] == 'EUR':
+                pair = marketplace.upper() + ':' + pair
+                eur_pairs.append(pair)
+                if markets_dict[market_pair]['leverage_buy'] != []:
+                    eur_margin_pairs.append(pair)
+                continue
+            if pair[-3:] == 'JPY':
+                pair = marketplace.upper() + ':' + pair
+                jpy_pairs.append(pair)
+                continue
+            if pair[-3:] == 'USD':
+                pair = marketplace.upper() + ':' + pair
+                usd_pairs.append(pair)
+                if markets_dict[market_pair]['leverage_buy'] != []:
+                    eur_margin_pairs.append(pair)
+                continue
+        btc_pairs.sort()
+        btc_margin_pairs.sort()
+        cad_pairs.sort()
+        eth_pairs.sort()
+        eth_margin_pairs.sort()
+        eur_pairs.sort()
+        eur_margin_pairs.sort()
+        jpy_pairs.sort()
+        usd_pairs.sort()
+        usd_margin_pairs.sort()
+        all_pairs = btc_pairs + eur_pairs + usd_pairs + eth_pairs + cad_pairs + jpy_pairs
+        all_margin_pairs = eur_margin_pairs + usd_margin_pairs + btc_margin_pairs \
+                           + eth_margin_pairs
+        #return all_pairs # I wasn't able to return data from exec in main, need to be solved
+        self.new_pairs_list = all_pairs
+        file_path = self.construct_file_path(marketplace, 'yes')
+        self.compare_pairs_list(marketplace, file_path, self.new_pairs_list)
 
     def main(self):
         """Call every marketplace functions.
@@ -301,7 +363,8 @@ class market_lists_updater():
         for marketplace in self.marketplace_obj:
             new_list = self.req_marketplace(marketplace)
             exec('self.' + marketplace + '(marketplace, new_list)')
-            self.compare_pairs_list(marketplace, self.new_pairs_list)
+            file_path = self.construct_file_path(marketplace)
+            self.compare_pairs_list(marketplace, file_path, self.new_pairs_list)
 
         print("Job done.")
 
